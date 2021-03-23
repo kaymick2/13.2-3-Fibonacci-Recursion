9e5cd6664dc814ffca1b8c500583b087175052f5
Kyle McDowell <kymcd21@icstudents.org>
2021-03-23 03:29:46 +0000

    final

--- Main.java
+++ Main.java
@@ -6,28 +6,27 @@ class Main {
     System.out.println("this program does fibonacci");
     wait(690);
     System.out.print("how many number u want: ");
-    int n = in.nextInt();
+    int termno = in.nextInt();
     long sum = 0;
 
-    for (int i = 1; i <= n; i++) {
-      long f = fib(i);
+    for (int i = 1; i <= termno; i++) {
+      long f = fibby(i);
 
-      System.out.println("fib(" + i + ") = " + f);
+      System.out.println("f(" + i + ") = " + f);
       sum = f + sum;
     }
     System.out.println("the sum is " + sum);
   }
 
-  public static long fib(int n) {
-    System.out.println("Entering fib: n = " + n);
-    if (n <= 2) {
+  public static long fibby(int termno) {
+    System.out.println("Entering fibby: term number = " + termno);
+    if (termno <= 2) {
       return 1;
     } else {
-      System.out.println("Exiting fib: n = " + n + " return value is below");
-      return fib(n - 1) + fib(n - 2);
+      System.out.println("Exiting fibby: term number = " + termno);
+      return fibby(termno - 1) + fibby(termno - 2);
     }
   }
-
   public static void wait(int ms) {
     try {
       Thread.sleep(ms);
