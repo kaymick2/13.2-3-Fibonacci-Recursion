15533059cbbdc191d435052b0a4d9ea2079a3689
Kyle McDowell <kymcd21@icstudents.org>
2021-03-23 03:21:18 +0000

    what is happening

--- Main.java
+++ Main.java
@@ -7,18 +7,25 @@ class Main {
     wait(690);
     System.out.print("how many number u want: ");
     int n = in.nextInt();
+    long sum = 0;
 
     for (int i = 1; i <= n; i++) {
       long f = fib(i);
+
       System.out.println("fib(" + i + ") = " + f);
+      sum = f + sum;
     }
+    System.out.println("the sum is " + sum);
   }
 
   public static long fib(int n) {
+    System.out.println("Entering fib: n = " + n);
     if (n <= 2) {
       return 1;
-    } else
+    } else {
+      System.out.println("Exiting fib: n = " + n + " return value is below");
       return fib(n - 1) + fib(n - 2);
+    }
   }
 
   public static void wait(int ms) {
